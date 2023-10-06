56c5bd88f12f1cc4f6e1730cf0cf081a7ef6446d
Xavier <boxia@andrew.cmu.edu>
2023-10-06 15:51:20 -0400

    reset to op

--- test/mocks/databasemock.js
+++ test/mocks/databasemock.js
@@ -253,7 +253,7 @@ async function enableDefaultPlugins() {
     const defaultEnabled = [
         'nodebb-plugin-dbsearch',
         'nodebb-widget-essentials',
-        'nodebb-plugin-composer-default',
+        'nodebb-plugin-composer-op',
     ].concat(testPlugins);
 
     winston.info('[install/enableDefaultPlugins] activating default plugins', defaultEnabled);
